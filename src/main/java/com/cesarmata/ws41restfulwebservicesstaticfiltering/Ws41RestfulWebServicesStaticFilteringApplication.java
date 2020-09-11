c7b20b3454d092dd9c8a009444ab2d624b4006a5
César <dev.pcesarmata@gmail.com>
2020-09-10 19:59:24 -0500

    Init commit.

---
+++ src/main/java/com/cesarmata/ws41restfulwebservicesstaticfiltering/Ws41RestfulWebServicesStaticFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws41restfulwebservicesstaticfiltering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws41RestfulWebServicesStaticFilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws41RestfulWebServicesStaticFilteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws41restfulwebservicesstaticfiltering/Ws41RestfulWebServicesStaticFilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws41restfulwebservicesstaticfiltering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws41RestfulWebServicesStaticFilteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
